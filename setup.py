c2c565c0961ceb2e40f56e05f6078ebf6d856fcb
John Clary <john.clary@austintexas.gov>
2018-12-16 13:25:43 -0600

    add install requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools
+    import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -9,6 +9,13 @@ setuptools.setup(
     author="City of Austin",
     author_email="transportation.data@austintexas.gov",
     description="Various utilities for managing and pulishing transportation data.",
+    install_requires=[
+      'arrow',
+      'requests',
+      'arcgis',
+      'pymssql',
+      'yagmail'
+    ],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cityofaustin/transportation-data-utils",
