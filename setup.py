0d68e5fa3c5389b94f3c7fd5efc3679f7bbd0b83
John Clary <john.clary@austintexas.gov>
2018-12-16 13:26:21 -0600

    bumpversion

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tdutils",
-    version="0.0.14",
+    version="0.0.15",
     author="City of Austin",
     author_email="transportation.data@austintexas.gov",
     description="Various utilities for managing and pulishing transportation data.",
